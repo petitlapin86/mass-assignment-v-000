b73fd192b443ee675ad0c0ea0f1b48e94115e9b3
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 18:45:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(hash)
-  attributes.each {|key, value| self.send(("#{key}="), value)}
+  hash.each {|key, value| self.send(("#{key}="), value)}
 end
 
 
